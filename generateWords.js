d416e3659f6757fc77b064f545bb6bd450309a65
jordankomnick <jordan@komnick.com>
2015-06-08 17:05:36 -0400

    Choose number of words/syllables

--- generateWords.js
+++ generateWords.js
@@ -1,36 +1,67 @@
+angular.module('generateWordsApp', []).controller(
+		'GeneratorController',
+		function() {
+			var generator = this;
+			generator.initials = [ {
+				text : "th"
+			}, {
+				text : "s"
+			}, {
+				text : "t"
+			} ];
+			generator.medials = [ {
+				text : "a"
+			}, {
+				text : "e"
+			} ];
+			generator.finals = [ {
+				text : "n"
+			}, {
+				text : "m"
+			} ];
+			generator.words = [];
+			generator.numWords = 20;
+			generator.minSyll = 1;
+			generator.maxSyll = 5;
 
-angular.module('generateWordsApp', [])
-  .controller('GeneratorController', function() {
-    var generator = this;
-    generator.initials = [{text: "th"}, {text: "s"}, {text: "t"}];
-    generator.medials = [{text: "a"}, {text: "e"}];
-    generator.finals = [{text: "n"}, {text: "m"}];
-    generator.words = [];
- 
-    generator.addInitial = function() {
-        generator.initials.push({text:generator.initialText});
-        generator.initialText = '';
-    };
-    
-    generator.addMedial = function() {
-    	generator.medials.push({text:generator.medialText});
-        generator.medialText = '';
-    };
-    
-    generator.addFinal = function() {
-    	generator.finals.push({text:generator.finalText});
-        generator.finalText = '';
-    };
-    
-    generator.generateWords = function() {
-    	for(var i = 0; i < 20; i++) {
-    		var initialRand = Math.floor(Math.random() * generator.initials.length); 
-    		var medialRand = Math.floor(Math.random() * generator.medials.length); 
-    		var finalRand = Math.floor(Math.random() * generator.finals.length);
-    		var word = generator.initials[initialRand].text + generator.medials[medialRand].text + generator.finals[finalRand].text;
-    		generator.words.push({text: word});
-    	}
-    	console.log(generator.words);
-    };
-  });
+			generator.addInitial = function() {
+				generator.initials.push({
+					text : generator.initialText
+				});
+				generator.initialText = '';
+			};
 
+			generator.addMedial = function() {
+				generator.medials.push({
+					text : generator.medialText
+				});
+				generator.medialText = '';
+			};
+
+			generator.addFinal = function() {
+				generator.finals.push({
+					text : generator.finalText
+				});
+				generator.finalText = '';
+			};
+
+			generator.generateWords = function() {
+				for (var i = 0; i < generator.numWords; i++) {
+					var word = "";
+					var numSyllRand = Math.floor(Math.random() * (generator.maxSyll - generator.minSyll + 1)) + generator.minSyll;
+					console.log(numSyllRand);
+					for (var j = 0; j < numSyllRand; j++) {
+						var initialRand = Math.floor(Math.random()
+								* generator.initials.length);
+						var medialRand = Math.floor(Math.random()
+								* generator.medials.length);
+						var finalRand = Math.floor(Math.random()
+								* generator.finals.length);
+						word += generator.initials[initialRand].text + generator.medials[medialRand].text + generator.finals[finalRand].text;
+					}
+					generator.words.push({
+						text : word
+					});
+				}
+			};
+		});
